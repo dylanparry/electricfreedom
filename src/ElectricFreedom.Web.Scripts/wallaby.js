8dae4d3e2842c6a52846bd38cb1f0525723a188f
Dylan Parry <me@dylanparry.com>
2017-01-23 15:21:52 +0000

    Remove overridden linting rules

--- src/ElectricFreedom.Web.Scripts/wallaby.js
+++ src/ElectricFreedom.Web.Scripts/wallaby.js
@@ -1,27 +1,27 @@
 module.exports = function wallabyConfig(wallaby) {
-    return {
-        files: [
-            'scripts/**/*.js',
-            'scripts/**/*.jsx',
-            '!scripts/**/tests/*.js',
-            '!scripts/**/tests/*.jsx',
-        ],
+  return {
+    files: [
+      'scripts/**/*.js',
+      'scripts/**/*.jsx',
+      '!scripts/**/tests/*.js',
+      '!scripts/**/tests/*.jsx',
+    ],
 
-        tests: [
-            'scripts/**/tests/*.js',
-            'scripts/**/tests/*.jsx',
-        ],
+    tests: [
+      'scripts/**/tests/*.js',
+      'scripts/**/tests/*.jsx',
+    ],
 
-        compilers: {
-            '**/*.js': wallaby.compilers.babel(),
-            '**/*.jsx': wallaby.compilers.babel(),
-        },
+    compilers: {
+      '**/*.js': wallaby.compilers.babel(),
+      '**/*.jsx': wallaby.compilers.babel(),
+    },
 
-        env: {
-            type: 'node',
-            runner: 'node',
-        },
+    env: {
+      type: 'node',
+      runner: 'node',
+    },
 
-        testFramework: 'jest',
-    };
+    testFramework: 'jest',
+  };
 };
--- src/ElectricFreedom.Web.Scripts/webpack.config.js
+++ src/ElectricFreedom.Web.Scripts/webpack.config.js
@@ -6,64 +6,64 @@ const webpack = require('webpack');
 
 // Shared config
 const sharedConfig = {
-    cache: true,
-    devtool: 'cheap-module-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: { // Override .babelrc to use ES2015 module syntax for tree shaking
-                    presets: [
-                        ['latest', {
-                            es2015: {
-                                modules: false,
-                            },
-                        }],
-                        'react',
-                    ],
-                    plugins: [
-                        'transform-decorators-legacy',
-                        'transform-class-properties',
-                    ],
-                },
-            },
-        ],
-    },
-    plugins: [],
+  cache: true,
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { // Override .babelrc to use ES2015 module syntax for tree shaking
+          presets: [
+            ['latest', {
+              es2015: {
+                modules: false,
+              },
+            }],
+            'react',
+          ],
+          plugins: [
+            'transform-decorators-legacy',
+            'transform-class-properties',
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [],
 };
 
 // Change some of the shared config if in production mode
 if (PRODUCTION) {
-    // Ensure we're building in production mode throughout
-    sharedConfig.plugins.push(new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: JSON.stringify('production'),
-        },
-    }));
+  // Ensure we're building in production mode throughout
+  sharedConfig.plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production'),
+    },
+  }));
 
-    // Use detailed source maps
-    sharedConfig.devtool = 'source-map';
+  // Use detailed source maps
+  sharedConfig.devtool = 'source-map';
 }
 
 // Define entry points, and outputs as an array
 const config = [
-    {
-        // Admin
-        entry: {
-            admin: './scripts/admin/src/bootstrap.jsx',
-        },
-        output: {
-            path: 'wwwroot/scripts',
-            filename: '[name].js',
-        },
+  {
+    // Admin
+    entry: {
+      admin: './scripts/admin/src/bootstrap.jsx',
     },
+    output: {
+      path: 'wwwroot/scripts',
+      filename: '[name].js',
+    },
+  },
 ];
 
 // Loop through the config entries, and extend each object with the shared config
 for (let i = 0; i < config.length; i += 1) {
-    Object.assign(config[i], sharedConfig);
+  Object.assign(config[i], sharedConfig);
 }
 
 module.exports = config;
