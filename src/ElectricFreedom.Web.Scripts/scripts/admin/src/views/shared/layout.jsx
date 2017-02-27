9a94bbad6810fbfeca9e93dc0b2472506d5bb370
Dylan Parry <me@dylanparry.com>
2017-02-27 17:30:07 +0000

    Add Link helper component - Store is always the same, so inject it instead of supplying it each time we create a Link

--- src/ElectricFreedom.Web.Scripts/scripts/admin/src/views/shared/layout.jsx
+++ src/ElectricFreedom.Web.Scripts/scripts/admin/src/views/shared/layout.jsx
@@ -1,49 +1,47 @@
 import React from 'react';
-import { inject, observer } from 'mobx-react';
-import { Link } from 'mobx-router';
 
 import routes from '../../config/routes';
+import Link from '../../components/link';
 
 const propTypes = {
   children: React.PropTypes.element.isRequired,
-  store: React.PropTypes.object.isRequired,
 };
 
-const layout = ({ children, store }) => (
+const layout = ({ children }) => (
   <div>
     <ul>
-      <li><Link view={routes.home} store={store}>Home</Link></li>
+      <li><Link view={routes.home}>Home</Link></li>
       <li>
-        <Link view={routes.artistIndex} store={store}>Artists</Link>
+        <Link view={routes.artistIndex}>Artists</Link>
         <ul>
-          <li><Link view={routes.artistAdd} store={store}>New Artist</Link></li>
-          <li><Link view={routes.artistEdit} params={{ artistId: 1 }} store={store}>Edit Artist</Link></li>
+          <li><Link view={routes.artistAdd}>New Artist</Link></li>
+          <li><Link view={routes.artistEdit} params={{ artistId: 1 }}>Edit Artist</Link></li>
           <li>
-            <Link view={routes.albumIndex} params={{ artistId: 1 }} store={store}>Albums</Link>
+            <Link view={routes.albumIndex} params={{ artistId: 1 }}>Albums</Link>
             <ul>
-              <li><Link view={routes.albumAdd} params={{ artistId: 1 }} store={store}>New Album</Link></li>
-              <li><Link view={routes.albumEdit} params={{ artistId: 1, albumId: 1 }} store={store}>Edit Album</Link></li>
+              <li><Link view={routes.albumAdd} params={{ artistId: 1 }}>New Album</Link></li>
+              <li><Link view={routes.albumEdit} params={{ artistId: 1, albumId: 1 }}>Edit Album</Link></li>
             </ul>
           </li>
         </ul>
       </li>
       <li>
-        <Link view={routes.articleIndex} store={store}>Articles</Link>
+        <Link view={routes.articleIndex}>Articles</Link>
         <ul>
-          <li><Link view={routes.articleAdd} store={store}>New Article</Link></li>
-          <li><Link view={routes.articleEdit} params={{ articleId: 1 }} store={store}>Edit Article</Link></li>
+          <li><Link view={routes.articleAdd}>New Article</Link></li>
+          <li><Link view={routes.articleEdit} params={{ articleId: 1 }}>Edit Article</Link></li>
         </ul>
       </li>
       <li>
-        <Link view={routes.tagIndex} store={store}>Tags</Link>
+        <Link view={routes.tagIndex}>Tags</Link>
         <ul>
-          <li><Link view={routes.tagAdd} store={store}>New Tag</Link></li>
-          <li><Link view={routes.tagEdit} params={{ tagId: 1 }} store={store}>Edit Tag</Link></li>
+          <li><Link view={routes.tagAdd}>New Tag</Link></li>
+          <li><Link view={routes.tagEdit} params={{ tagId: 1 }}>Edit Tag</Link></li>
         </ul>
       </li>
-      <li><Link view={routes.reviewIndex} store={store}>Reviews</Link></li>
-      <li><Link view={routes.commentIndex} store={store}>Comments</Link></li>
-      <li><Link view={routes.userIndex} store={store}>Users</Link></li>
+      <li><Link view={routes.reviewIndex}>Reviews</Link></li>
+      <li><Link view={routes.commentIndex}>Comments</Link></li>
+      <li><Link view={routes.userIndex}>Users</Link></li>
     </ul>
 
     {children}
@@ -52,4 +50,4 @@ const layout = ({ children, store }) => (
 
 layout.propTypes = propTypes;
 
-export default inject('store')(observer(layout));
+export default layout;
