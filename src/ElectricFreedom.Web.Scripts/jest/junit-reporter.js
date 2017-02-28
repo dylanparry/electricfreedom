e4142339d9f1559cc074d98b809e5037dd39c7f7
Dylan Parry <me@dylanparry.com>
2017-02-28 14:25:46 +0000

    Remove Jasmine reporters

--- src/ElectricFreedom.Web.Scripts/jest/junit-reporter.js
+++
@@ -1,9 +0,0 @@
-const reporters = require('jasmine-reporters');
-
-const reporter = new reporters.JUnitXmlReporter({
-    consolidateAll: false,
-    filePrefix: 'jest-junit-result-',
-    savePath: 'jest/results/',
-});
-
-jasmine.getEnv().addReporter(reporter); // eslint-disable-line no-undef
