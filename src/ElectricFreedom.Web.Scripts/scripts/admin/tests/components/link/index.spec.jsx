0bd71e87dff8e7c10b382bbfc0f5cca04baa4693
Dylan Parry <me@dylanparry.com>
2017-02-28 21:44:53 +0000

    Add tests for Link component

---
+++ src/ElectricFreedom.Web.Scripts/scripts/admin/tests/components/link/index.spec.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { RouterStore, Route } from 'mobx-router';
+import { shallow } from 'enzyme';
+
+import Link from '../../../src/components/link';
+
+describe('<Link /> component', () => {
+  const store = {
+    router: new RouterStore(),
+  };
+  const routes = {
+    basic: new Route({
+      path: '/test',
+      component: null,
+    }),
+    withParams: new Route({
+      path: '/test/:param',
+    }),
+  };
+
+  it('Renders a basic link with correct path and text', () => {
+    const sut = shallow(
+      <Link view={routes.basic} store={store}>Test Link</Link>
+    );
+
+    expect(sut.html()).toBe('<a href="/test">Test Link</a>');
+  });
+
+  it('Renders a paramaterised link with correct path and text', () => {
+    const sut = shallow(
+      <Link view={routes.withParams} params={{ param: 123 }} store={store}>Test Link</Link>
+    );
+
+    expect(sut.html()).toBe('<a href="/test/123">Test Link</a>');
+  });
+});
