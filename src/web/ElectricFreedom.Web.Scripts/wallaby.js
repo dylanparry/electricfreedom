1c516578baf72e5aa38a272bd90e256d8a887ca1
Dylan Parry <me@dylanparry.com>
2017-03-24 14:08:00 +0000

    Include snap files

--- src/web/ElectricFreedom.Web.Scripts/wallaby.js
+++ src/web/ElectricFreedom.Web.Scripts/wallaby.js
@@ -4,6 +4,7 @@ module.exports = function wallabyConfig(wallaby)
     files: [
       'scripts/**/*.js',
       'scripts/**/*.jsx',
+      'scripts/**/*.snap',
       '!scripts/**/__tests__/*.js',
       '!scripts/**/__tests__/*.jsx',
     ],
