e07b1b8b989094ff9305adf573e71647b2a4561e
Dylan Parry <me@dylanparry.com>
2017-03-27 12:40:15 +0100

    Add tests for route matching

--- src/web/ElectricFreedom.Web.Scripts/scripts/admin/views/album/__tests__/index.spec.jsx
+++ src/web/ElectricFreedom.Web.Scripts/scripts/admin/views/album/__tests__/index.spec.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 
 import AlbumSwitch from '../index';
+import AlbumAdd from '../add';
+import AlbumEdit from '../edit';
+import AlbumList from '../list';
 
-const setup = () =>
+const setup = (propOverrides) =>
 {
   const wrapper = shallow(<AlbumSwitch />);
 
+  const props = Object.assign({
+    initialEntries: ['/'],
+  }, propOverrides);
+  const mounted = mount(<MemoryRouter {...props}><AlbumSwitch /></MemoryRouter>);
+
   return {
     wrapper,
+    mounted,
   };
 };
 
@@ -19,5 +29,44 @@ describe('AlbumSwitch', () =>
     const { wrapper } = setup();
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('route "/artists/:artistId/albums/add" renders AlbumAdd', () =>
+  {
+    for (let i = 1; i <= 10; i += 1)
+    {
+      const { mounted } = setup({
+        initialEntries: [`/artists/${i}/albums/add`],
+      });
+
+      expect(mounted.find(AlbumAdd).exists()).toBe(true);
+    }
+  });
+
+  test('route "/artists/:artistId/albums/:albumId" renders AlbumEdit', () =>
+  {
+    for (let i = 1; i <= 10; i += 1)
+    {
+      for (let j = 1; j <= 10; j += 1)
+      {
+        const { mounted } = setup({
+          initialEntries: [`/artists/${i}/albums/${j}`],
+        });
+
+        expect(mounted.find(AlbumEdit).exists()).toBe(true);
+      }
+    }
+  });
+
+  test('route "/artists/:artistId/albums" renders AlbumList', () =>
+  {
+    for (let i = 1; i <= 10; i += 1)
+    {
+      const { mounted } = setup({
+        initialEntries: [`/artists/${i}/albums`],
+      });
+
+      expect(mounted.find(AlbumList).exists()).toBe(true);
+    }
   });
 });
